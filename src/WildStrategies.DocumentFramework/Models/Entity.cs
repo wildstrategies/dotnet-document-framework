16cedd31e9ca1a6da722e7473132cd9e6fac5b06
Nicola Luraghi <a3nick@users.noreply.github.com>
2022-12-04 18:06:41 +0100

    Removes protected set (#43)

--- src/WildStrategies.DocumentFramework/Models/Entity.cs
+++ src/WildStrategies.DocumentFramework/Models/Entity.cs
@@ -1,10 +1,10 @@
-﻿
+
 using System.ComponentModel.DataAnnotations;
 
 namespace WildStrategies.DocumentFramework
 {
     public abstract class Entity : ValueObject, ILastUpdateTimeProvider, IEntity
     {
-        [Required] public DateTime LastUpdateTime { get; protected set; } = DateTime.UtcNow;
+        [Required] public DateTime LastUpdateTime { get; init; } = DateTime.UtcNow;
     }
 }
